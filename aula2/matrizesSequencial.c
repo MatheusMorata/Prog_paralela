e117da1f86f3ee1d93a9513f28eacf7e2e0301af
MatheusMorata <matlucar09@hotmail.com>
2024-10-15 09:10:32 -0300

    FEAT: modularizei matrizesSequencial.c

--- aula2/matrizesSequencial.c
+++ aula2/matrizesSequencial.c
@@ -4,12 +4,25 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#define N 1000000000 // Tamanho das matrizes 
+#define N 3 // Tamanho das matrizes 
 
+// Função para povoar as matrizes
 void preencher(int matriz[N][N]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             matriz[i][j] = 1; // Populando as matrizes
+        }
+    }
+}
+
+// Função que multiplica as matrizes sequencialmente
+void multiplicarMatrizes(int a[N][N], int b[N][N], int c[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            c[i][j] = 0; 
+            for (int k = 0; k < N; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
         }
     }
 }
@@ -22,13 +35,6 @@ void main() {
     preencher(a);
     preencher(b);
 
-    // Mutliplicando as matrizes
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            c[i][j] = 0; // Reset the value
-            for (int k = 0; k < N; k++) {
-                c[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
+    multiplicarMatrizes(a, b, c);
+
 }
