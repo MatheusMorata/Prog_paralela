80aca516b09513d841f84976eea27e7f5495a52c
MatheusMorata <matlucar09@hotmail.com>
2024-10-15 09:18:19 -0300

    FEAT: modularizando matrizesParalelo.c

--- aula2/matrizesParalelo.c
+++ aula2/matrizesParalelo.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <omp.h> // Incluir OpenMP
+#include <omp.h> // Importando OPENMP
 
-#define N 2 // Definição da constante N
+#define N 2 // Tamanho das matrizes
 
 void preencher(int matriz[N][N]) {
     for (int i = 0; i < N; i++) {
@@ -12,7 +12,20 @@ void preencher(int matriz[N][N]) {
     }
 }
 
-int main() {
+// Multiplicar matrizes a e b em paralelo
+void multiplicarMatrizesParalelo(int a[N][N], int b[N][N], int c[N][N]) {
+    #pragma omp parallel for
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            c[i][j] = 0; 
+            for (int k = 0; k < N; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+void main() {
     int a[N][N];
     int b[N][N];
     int c[N][N]; 
@@ -20,25 +33,6 @@ int main() {
     preencher(a);
     preencher(b);
 
-    // Multiplicar matrizes a e b em paralelo
-    #pragma omp parallel  // Diretriz para paralelismo
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            c[i][j] = 0; // Resetar o valor
-            for (int k = 0; k < N; k++) {
-                c[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
+    multiplicarMatrizesParalelo(a, b, c);
 
-    // Imprimir o resultado
-    printf("Matriz resultante c:\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d ", c[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
 }
