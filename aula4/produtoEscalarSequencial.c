0ad9eaaae59caffe5331f3e8aba42226e422ccb9
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2024-10-23 15:58:52 -0300

    REFACTOR: Alteração para getTimeOfDay

--- aula4/produtoEscalarSequencial.c
+++ aula4/produtoEscalarSequencial.c
@@ -1,26 +1,41 @@
-/* 
-    Cálculo do produto escalar de forma sequencial
-*/
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
+#include <sys/time.h>
 
-void main(){
+/*
+Calculando produto escalar de forma sequencial
+*/
+int main() {
     double sum = 0;
-    double a[99999], b[99999];
-    int n = 99999;
-    
-    for(int i = 0; i<n; i++){
+    double a[100000], b[100000];
+    int n = 100000;
+    struct timeval start, end;
+
+  
+    for (int i = 0; i < n; i++) {
         a[i] = i * 0.5;
         b[i] = i * 2;
     }
-    double inicio = omp_get_wtime();
-    for(int i = 1; i<n; i++){
-        sum = sum + a[i] * b[i];
-    }
-    double resultado = omp_get_wtime() - inicio; 
 
-    printf("Soma = %.2f\n",sum);
-    printf("Tempo = %.8f\n",resultado);
+    // Captura o tempo de início
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < n; i++) {
+        sum += a[i] * b[i];
+    }
+
+    // Captura o tempo de fim
+    gettimeofday(&end, NULL);
+
+    // Calcula a diferença em microsegundos
+    long seconds = end.tv_sec - start.tv_sec;
+    long microseconds = end.tv_usec - start.tv_usec;
+    double elapsed = seconds + microseconds * 1e-6;
+
+
+    printf("Soma = %.2f\n", sum);
+    printf("Tempo de execução: %.8f segundos\n", elapsed);
+
+    return 0;
 }
