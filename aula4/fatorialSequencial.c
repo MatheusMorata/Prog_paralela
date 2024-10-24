88aeea7fd6d4ca8968677a01983d1a0560621bac
MatheusMorata <matlucar09@hotmail.com>
2024-10-24 16:02:32 -0300

    FEAT: Concluido fatorialSequencial.c

---
+++ aula4/fatorialSequencial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(){
+    int fatorial = 1;
+    int n = 10;
+
+    // Captura o tempo de início
+    double start_time = omp_get_wtime();
+    for(int i = 2; i <= n; i++){
+        fatorial *= i;
+    }
+    // Captura o tempo de fim
+    double sequencial_time = omp_get_wtime() - start_time;
+
+
+    printf("Fatorial de %d = %d\n",n,fatorial);
+    printf("Tempo de execucao: %.8f segundos\n", sequencial_time);
+    return 0;
+}
