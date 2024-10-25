8955e99a6def893a098e6e353a8426f054be6309
MatheusMorata <matlucar09@hotmail.com>
2024-10-25 10:00:06 -0300

    ADD: FatorialCritical.c adicionado

---
+++ aula4/fatorialCritical.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+
+void main() {
+    int i;
+    int n = 170;
+    double fac = 1;
+
+    // Captura o tempo de início
+    double start_time = omp_get_wtime();
+
+    #pragma omp parallel for private(i)
+    for (i = 2; i <= n; i++) {
+        #pragma omp critical
+        {
+            fac *= i;
+        }
+    }
+
+    // Captura o tempo de fim
+    double sequencial_time = omp_get_wtime() - start_time;
+
+    printf("Fatorial de %d = %.0f", n, fac);
+    printf("\nTempo de execucao: %.8f\n", sequencial_time);
+}
