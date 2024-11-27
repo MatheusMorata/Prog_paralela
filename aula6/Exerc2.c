d72e54ed115e175a0bb80d3aae82d218997e0508
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2024-11-27 12:17:24 -0300

    FEAT: Mudanças no contador

--- aula6/Exerc2.c
+++ aula6/Exerc2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NUM_MESSAGES 100
+#define NUM_MENSAGENS 100
 
 int main(int argc, char* argv[]) {
     int rank, size;
@@ -14,33 +14,33 @@ int main(int argc, char* argv[]) {
     // Verificar se o nome do arquivo foi fornecido
     if (argc != 2) {
         if (rank == 0) {
-            printf("Usage: %s <output_filename>\n", argv[0]);
+            printf("Uso: %s <nome_do_arquivo_saida>\n", argv[0]);
         }
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    const char* filename = argv[1];
+    const char* nome_arquivo = argv[1];
 
     if (rank == 0) {
         // Processo zero: Recebe mensagens e escreve no arquivo de saída
-        FILE* output = fopen(filename, "w");
+        FILE* output = fopen(nome_arquivo, "w");
         if (output == NULL) {
-            printf("Error opening file!\n");
+            printf("Erro ao abrir o arquivo!\n");
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
 
-        for (int i = 0; i < (size - 1) * NUM_MESSAGES; i++) {
-            int message;
-            MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
-            fprintf(output, "Received message %d from process %d\n", message, stat.MPI_SOURCE);
+        for (int i = 0; i < (size - 1) * NUM_MENSAGENS; i++) {
+            int mensagem;
+            MPI_Recv(&mensagem, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+            fprintf(output, "Mensagem recebida %d do processo %d\n", mensagem, stat.MPI_SOURCE);
         }
 
         fclose(output);
     } else {
         // Outros processos: Enviam 100 mensagens para o processo zero
-        for (int i = 0; i < NUM_MESSAGES; i++) {
-            int message = rank * 100 + i; // Mensagem pode ser qualquer valor
-            MPI_Send(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        for (int i = 0; i < NUM_MENSAGENS; i++) {
+            int mensagem = i; // Mensagem começa do zero
+            MPI_Send(&mensagem, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
         }
     }
 
